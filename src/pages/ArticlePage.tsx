dfe4cdf71b2869ce778d983adfdff591ba1ac8f3
Мария Тимашова <Mary-fox2008@yandex.ru>
2023-07-28 16:37:59 +0300

    refactor: add a separate component ArticleItem

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { action } from "mobx";
 import { observer } from "mobx-react";
-import styled from "styled-components";
-import ArticlesStore from "../ArticlesStore";
+import ArticlesStore from "../store/ArticlesStore";
 import Navbar from "../components/Navbar";
-import Comments from "../components/Comments";
 import Loader from "../components/Loader";
 import ErrorPage from "./ErrorPage";
-
-const ArticleWrapper = styled.div`
-  color: white;
-  padding: 20px;
-`;
-const ArticleHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-const ArticleLink = styled.a`
-  color: white;
-  font-size: 20px;
-  background-color: rgba(57, 57, 57, 0.5);
-  border-radius: 10%;
-  padding: 5px;
-  &:hover {
-    color: #cdcd3cd1;
-    cursor: pointer;
-  }
-`;
+import ArticleItem from "../components/ArticleItem";
 
 const ArticlePage: React.FC = observer(() => {
   const { storyId } = useParams<{ storyId: string }>();
-  const [refreshFlag, setRefreshFlag] = useState(false);
 
   useEffect(() => {
     const id = Number(storyId);
@@ -50,27 +27,11 @@ const ArticlePage: React.FC = observer(() => {
   if (!story) {
     return <ErrorPage />;
   }
-  const handleRefreshComments = action(() => {
-    ArticlesStore.fetchArticleComments(story.id); // Обновляем все комментарии
-    setRefreshFlag(!refreshFlag);
-  });
 
   return (
     <div>
       <Navbar />
-      <ArticleWrapper>
-        <ArticleHeader>
-          <p>Author: {story.by}</p>
-          <p>{story.time ? new Date(story.time * 1000).toUTCString() : ""}</p>
-        </ArticleHeader>
-        <h1>{story.title}</h1>
-        <ArticleLink href={story.url} target="_blank" rel="noopener noreferrer">
-          Open the article
-        </ArticleLink>
-        <p>Comments: {story.descendants}</p>
-        <button onClick={handleRefreshComments}>Обновить комментарии</button>
-        <Comments comments={story.kids ?? []} refreshFlag={refreshFlag} />
-      </ArticleWrapper>
+      <ArticleItem story={story} />
     </div>
   );
 });
